66754b6e6e62bf0c721e32d14752c57b3858e1fd
liuaiqi <liuaiqi@liuaiqideMacBook-Pro.local>
2016-01-25 10:56:56 +0800

    生成一个n位随机字符串OC

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def percent(s):
     return "%.2f%%" % (s*100)
 
 
-# -------------------------------------
+# ------------------------------------
 
 from collections import OrderedDict
 def dedup_list(li):
@@ -15,5 +15,15 @@ def dedup_list(li):
     list去重(保持顺序)
     """
     return list(OrderedDict.fromkeys(li))
+
+# ------------------------------------
+
+import string
+import random
+def rand_str_n(n):
+    """
+    生成一个n位随机字符串,包含数字和字母
+    """
+    return ''.join([(string.ascii_letters+string.digits)[x] for x in random.sample(range(0,62),n)])
 
 # ------------------------------------
